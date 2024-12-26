3b840248918b680ea255911076f3e88cb342ea0f
Papabdou12 <abdouniang30@gmail.com>
2024-12-26 14:08:02 +0000

    first spring react projct

---
+++ src/test/java/com/FullStack_SpringBoot_React/FullStackSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FullStack_SpringBoot_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackSpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
